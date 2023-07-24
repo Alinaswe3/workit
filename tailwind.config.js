d297a84108eeabcc058f0e74083e15c0ae62641a
Alinaswe <85223818+Alinaswe3@users.noreply.github.com>
2023-07-25 00:51:38 +0200

    Added default styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ["./src/**/*.{html,js,svelte,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "dark-purple": "#24053e",
+        eucaplyptus: "#44ffa1",
+        "davys-greya": "#584d62",
+        "ghost-white": "#fcf8ff",
+      },
+      fontSize: {
+        "h-sm": "3.2rem",
+        "h-md": "5.6rem",
+        "h-lg": "8rem",
+        "body-base": "1.8rem",
+      },
+    },
   },
   plugins: [],
 };
