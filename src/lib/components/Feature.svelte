867ae6f203807acf8a21f72b639d7eb04fd07892
Alinaswe <85223818+Alinaswe3@users.noreply.github.com>
2023-07-25 13:11:00 +0200

    Added features section and feature markup

---
+++ src/lib/components/Feature.svelte
@@ -0,0 +1,16 @@
+<script>
+    export let number = 1;
+    export let heading = "Heading";
+</script>
+
+<div class="flex flex-col">
+    <div>
+        {number}
+    </div>
+    <div>
+        <h3>{heading}</h3>
+        <p>
+            <slot/>
+        </p>
+    </div>
+</div>
---
+++ src/lib/components/Features.svelte
@@ -0,0 +1,13 @@
+<script>
+    import Container from "$lib/components/Container.svelte";
+    import Feature from "$lib/components/Feature.svelte";
+</script>
+
+<section>
+    <Container>
+        <Feature>
+            Optimize your products, improve customer satisfaction and stay ahead of the competition with our product
+            data analytics.
+        </Feature>
+    </Container>
+</section>
