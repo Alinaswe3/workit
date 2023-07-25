6cb714007e9123a6788cc4a6cba9201c1eae7398
Alinaswe <85223818+Alinaswe3@users.noreply.github.com>
2023-07-25 12:14:40 +0200

    Added hero content

--- src/lib/components/Container.svelte
+++ src/lib/components/Container.svelte
@@ -1,3 +1,7 @@
-<div class="max-w-[112rem] mx-auto px-8 h-full">
+<script>
+    export let cssClasses = "";
+</script>
+
+<div class={`max-w-[112rem] mx-auto px-8 h-full ${cssClasses}`}>
     <slot/>
 </div>
--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -5,8 +5,12 @@
 </script>
 
 <section class="min-h-fit h-screen w-full min-w-fit bg-dark-purple">
-    <Container>
+    <Container cssClasses="grid grid-rows-[max-content_1fr]">
         <NavBar/>
+        <div class="text-white flex flex-col items-center gap-16 justify-self-center self-center -translate-y-1/4">
+            <h1 class="text-[8rem] text-center">Data <span class="underline underline-offset-8 decoration-eucaplyptus">tailored</span> to<br/> your needs.</h1>
+            <button class="tracking-wide transition-colors duration-300 hover:bg-dark-purple hover:text-eucaplyptus bg-eucaplyptus px-8 py-5 text-dark-purple font-bold text-[1.6rem] border-2 border-eucaplyptus">Learn more</button>
+        </div>
     </Container>
     <BottomCurve/>
 </section>
