3efa58a8a85006802b5798eb07fea399aececd6c
Alinaswe <85223818+Alinaswe3@users.noreply.github.com>
2023-07-25 10:53:39 +0200

    Added bottom curve to hero section

---
+++ src/lib/components/Hero.svelte
@@ -0,0 +1,8 @@
+<script>
+    import BottomCurve from "$lib/components/BottomCurve.svelte";
+</script>
+
+<section class="min-h-fit h-screen w-full min-w-fit bg-dark-purple">
+
+    <BottomCurve />
+</section>
