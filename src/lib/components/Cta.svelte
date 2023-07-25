a5139b475c183c285676d3011aff476d72a785b9
Alinaswe <85223818+Alinaswe3@users.noreply.github.com>
2023-07-25 22:09:30 +0200

    Added cta markup

---
+++ src/lib/components/Cta.svelte
@@ -0,0 +1,14 @@
+<section>
+    <div>
+        <img src="/image-founder.webp" alt="Founder of workit Louis Graham"/>
+    </div>
+    <div>
+        <h2>Be the first to test</h2>
+        <p>Hi, I'm Louis Graham, the founder of the company. Book a demo call with me to become a beta tester for our
+            app and kickstart your company. Apply for access below and I’ll be in touch to schedule a call.</p>
+        <button class="btn">
+            Apply for access
+        </button>
+        <img src="/bg-pattern-3.svg" alt="Decoration coils"/>
+    </div>
+</section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script>
     import Hero from "$lib/components/Hero.svelte";
     import Features from "$lib/components/Features.svelte";
+    import Cta from "$lib/components/Cta.svelte";
 </script>
 
 <Hero/>
 <Features/>
+<Cta/>
