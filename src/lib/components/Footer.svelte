bce7b16dd180a834921d7ef877f807073fb4f0a1
Alinaswe <85223818+Alinaswe3@users.noreply.github.com>
2023-07-26 00:38:12 +0200

    Built the footer

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,29 @@
+<script>
+    import Container from "$lib/components/Container.svelte";
+    import FacebookIcon from "$lib/components/icons/FacebookIcon.svelte";
+    import TwitterIcon from "$lib/components/icons/TwitterIcon.svelte";
+    import InstagramIcon from "$lib/components/icons/InstagramIcon.svelte";
+</script>
+
+<footer class="mb-20">
+    <Container cssClasses="flex justify-center items-center">
+        <div class="flex flex-col items-center gap-16">
+            <img class="max-w-[10rem]" src="/logo-dark.svg" alt="Dark version of company logo"/>
+            <div class="flex gap-16 items-center">
+                <a href="/">
+
+                    <FacebookIcon/>
+                </a> <a href="/">
+
+                <TwitterIcon/>
+            </a>
+                <a href="/">
+                    <InstagramIcon/>
+                </a>
+
+            </div>
+        </div>
+
+
+    </Container>
+</footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,10 @@
     import Hero from "$lib/components/Hero.svelte";
     import Features from "$lib/components/Features.svelte";
     import Cta from "$lib/components/Cta.svelte";
+    import Footer from "$lib/components/Footer.svelte";
 </script>
 
 <Hero/>
 <Features/>
 <Cta/>
+<Footer/>
