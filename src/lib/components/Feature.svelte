d51c129c3612d1781385faa4d43d61ae44151c33
Alinaswe <85223818+Alinaswe3@users.noreply.github.com>
2023-07-25 21:35:15 +0200

    Styled the features

--- src/lib/components/Feature.svelte
+++ src/lib/components/Feature.svelte
@@ -3,13 +3,13 @@
     export let heading = "Heading";
 </script>
 
-<div class="flex flex-col">
-    <div>
-        {number}
+<div class="flex flex-col items-center text-center gap-16">
+    <div class="flex justify-center items-center w-[5.6rem] h-[5.6rem] rounded-full border border-dark-purple">
+        <h3 class="text-[2.4rem]">{number}</h3>
     </div>
     <div>
-        <h3>{heading}</h3>
-        <p>
+        <h2 class="text-[3.2rem]">{heading}</h2>
+        <p class="text-[1.8rem]">
             <slot/>
         </p>
     </div>
--- src/lib/components/Features.svelte
+++ src/lib/components/Features.svelte
@@ -5,9 +5,21 @@
 
 <section>
     <Container>
-        <Feature>
-            Optimize your products, improve customer satisfaction and stay ahead of the competition with our product
-            data analytics.
-        </Feature>
+        <div class="grid grid-cols-3 gap-10">
+
+            <Feature heading="Actionable insights">
+                Optimize your products, improve customer satisfaction and stay ahead of the competition with our product
+                data analytics.
+            </Feature>
+            <Feature number=2 heading="Data-driven decisions">
+                Make data-driven decisions with our product data analytics. Our AI-generated reports help you unlock
+                insights hidden in your product data.
+            </Feature>
+            <Feature number=3 heading="Always affordable">
+
+                Always affordable pricing that scales with your business. Get top-quality product data analytics
+                services without hidden costs or unexpected fees.
+            </Feature>
+        </div>
     </Container>
 </section>
