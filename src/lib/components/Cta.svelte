20af236cc185f142edbc4629ee6dd58191400203
Alinaswe <85223818+Alinaswe3@users.noreply.github.com>
2023-07-26 11:29:40 +0200

    Made cta image take up full space of outer div

--- src/lib/components/Cta.svelte
+++ src/lib/components/Cta.svelte
@@ -6,7 +6,7 @@
     <Container>
     <div class="ml-auto max-w-[75%] md:max-w-full sm:text-center sm:items-center flex flex-col items-start gap-10 relative bg-dark-purple text-white p-[6.4rem] sm:p-[3.2rem]">
         <div class="-z-10 md:left-1/2 md:-translate-x-1/2 -left-[42%] -top-[53%] md:-top-[24rem] absolute bg-yellow-500 min-w-[28rem] min-h-[28rem] sm:w-[32rem] sm:h-[32rem] w-[42rem] h-[42rem] overflow-hidden rounded-full">
-            <img src="/image-founder.webp" alt="Founder of workit Louis Graham"/>
+            <img class="w-full h-fullS" src="/image-founder.webp" alt="Founder of workit Louis Graham"/>
         </div>
         <h2 class="leading-none text-h-md sm:text-[4rem]">Be the first to test</h2>
         <p class="text-body-base">Hi, I'm Louis Graham, the founder of the company. Book a demo call with me to become a beta tester for our
