4b8c30849cf301badb2551632dd000ea669100da
Alinaswe <85223818+Alinaswe3@users.noreply.github.com>
2023-07-26 10:18:55 +0200

    Increased padding of container

--- src/lib/components/Container.svelte
+++ src/lib/components/Container.svelte
@@ -2,6 +2,6 @@
     export let cssClasses = "";
 </script>
 
-<div class={`max-w-[112rem] mx-auto px-8 h-full ${cssClasses}`}>
+<div class={`max-w-[112rem] mx-auto px-16 h-full ${cssClasses}`}>
     <slot/>
 </div>
